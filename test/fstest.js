431f6e8f2ab7e32d3320564ca14d2ae5429c57d8
inpa12 <inpa12@gmail.com>
2025-01-31 16:41:03 +0900

    nodevue 추가 2

--- test/fstest.js
+++ test/fstest.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
+const util = require('util');
 
-fs.readFile('test.json', 'utf-8', (err, data) => {
+
+fs.readFile(__dirname + '\\test.json', 'utf-8', (err, data) => {
         if (err) return console.error(err);
 
         console.log("data>>", data);
@@ -8,7 +10,14 @@ fs.readFile('test.json', 'utf-8', (err, data) => {
 
 console.log("------------------------");
 
-let data2 = fs.readFileSync('test.json', 'utf-8');
+const msgfile = __dirname + "\\message.txt"
+fs.writeFileSync(msgfile, 'Hello Node.js 세종대왕', (err) => {
+  if (err) throw err;
+  console.log('The file has been saved!');
+}); 
+
+let data2 = fs.readFileSync(msgfile, 'utf-8');
 console.log("data2>>", data2);
 
 console.log("===================================");
+
